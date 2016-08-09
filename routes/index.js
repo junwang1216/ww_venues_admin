5010962831656ddbca9f2526b9c8858080a55594
jun.wang <jun.wang@jimubox.com>
2016-08-09 18:22:05 +0800

    feature(demo2): 场地设置

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', Home.renderHomeIndex);
 
 router.get('/venue/sequence', Venue.renderVenueSequenceReserve);
 router.get('/venue/sequencePDA', Venue.renderVenueSequencePDA);
-
+router.get('/venue/sports', Venue.renderVenueSportsSettings);
+router.get('/venue/sites', Venue.renderVenueSitesSettings);
 //对外接口
 exports.root = router;
--- routes/venue/index.js
+++ routes/venue/index.js
@@ -11,4 +11,12 @@ Venue_Controller.renderVenueSequencePDA = function (req, res) {
     res.render('venue/sequence_pda');
 };
 
+Venue_Controller.renderVenueSportsSettings = function (req, res) {
+    res.render('venue/sports_settings');
+};
+
+Venue_Controller.renderVenueSitesSettings = function (req, res) {
+    res.render('venue/sites_settings');
+};
+
 module.exports = Venue_Controller;
