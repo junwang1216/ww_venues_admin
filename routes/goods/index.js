eed8adebe3286ed008a6f5f356c2d13710a4285a
jun.wang <jun.wang@jimubox.com>
2016-09-22 13:58:06 +0800

    feature(goods): 商品管理页面

--- routes/goods/index.js
+++ routes/goods/index.js
@@ -22,6 +22,7 @@ Goods_Controller.renderGoodsSettings = function (req, res) {
 
 // 商品设置提交
 Goods_Controller.submitGoodsSettings = function (req, res) {
+    res.redirect('/goods/stockManagement');
 };
 
 // 商品进销存管理
@@ -29,7 +30,7 @@ Goods_Controller.renderGoodsStockManagement = function (req, res) {
     res.render('goods/goods_stock_management', {
         page: Goods_Controller.NavPage,
         goods: [{
-            id: 1,
+            good_id: 1,
             good_no: "GD10002",
             good_name: "羽毛球拍",
             good_count: 11,
@@ -37,7 +38,7 @@ Goods_Controller.renderGoodsStockManagement = function (req, res) {
             discount_price: "95.00",
             good_state: 1
         }, {
-            id: 2,
+            good_id: 2,
             good_no: "GD10001",
             good_name: "农夫山泉矿泉水",
             good_count: 1100,
@@ -45,21 +46,91 @@ Goods_Controller.renderGoodsStockManagement = function (req, res) {
             discount_price: "2.00",
             good_state: 2
         }, {
-            id: 3,
+            good_id: 3,
             good_no: "GD10003",
             good_name: "溜冰鞋",
             good_count: 11,
             good_price: "182.00",
             discount_price: "175.00",
             good_state: 3
-        }]
+        }, {
+            good_id: 4,
+            good_no: "GD10004",
+            good_name: "游泳帽",
+            good_count: 100,
+            good_price: "23.00",
+            discount_price: "20.00",
+            good_state: 1
+        }, {
+            good_id: 5,
+            good_no: "GD10005",
+            good_name: "游泳帽",
+            good_count: 1100,
+            good_price: "2.50",
+            discount_price: "2.00",
+            good_state: 2
+        }, {
+            good_id: 6,
+            good_no: "GD10006",
+            good_name: "溜冰鞋",
+            good_count: 11,
+            good_price: "182.00",
+            discount_price: "175.00",
+            good_state: 3
+        }, {
+            good_id: 7,
+            good_no: "GD10007",
+            good_name: "羽毛球拍",
+            good_count: 11,
+            good_price: "100.00",
+            discount_price: "95.00",
+            good_state: 1
+        }, {
+            good_id: 8,
+            good_no: "GD10008",
+            good_name: "农夫山泉矿泉水",
+            good_count: 1100,
+            good_price: "2.50",
+            discount_price: "2.00",
+            good_state: 2
+        }, {
+            good_id: 9,
+            good_no: "GD10009",
+            good_name: "溜冰鞋",
+            good_count: 11,
+            good_price: "182.00",
+            discount_price: "175.00",
+            good_state: 3
+        }, {
+            good_id: 10,
+            good_no: "GD10010",
+            good_name: "羽毛球拍",
+            good_count: 11,
+            good_price: "100.00",
+            discount_price: "95.00",
+            good_state: 1
+        }],
+        pagination: {
+            index: 1,
+            pageSize: 10,
+            total: 143
+        }
     });
 };
 
 // 商品详情
 Goods_Controller.renderGoodsStockView = function (req, res) {
     res.render('goods/goods_stock_view', {
-        page: Goods_Controller.NavPage
+        page: Goods_Controller.NavPage,
+        good: {
+            good_id: 10,
+            good_no: "GD10010",
+            good_name: "羽毛球拍",
+            good_count: 11,
+            good_price: "100.00",
+            discount_price: "95.00",
+            good_state: 1
+        }
     })
 };
 
